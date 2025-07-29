99e71a399c78b0eed798214e4c75df8f25fa0c9e
999nike <999nike@gmail.com>
2025-07-29 21:41:02 +0100

    Delete public directory

--- public/scripts/rules.js
+++
@@ -1,7 +0,0 @@
-let overrideMode = false; let devMasterActive = true;
-function applyOverrideMode() { overrideMode = true; localStorage.setItem("wizzOverride", "true"); }
-function clearOverrideMode() { overrideMode = false; localStorage.removeItem("wizzOverride"); }
-function isOverrideModeActive() { return overrideMode; }
-function enforceDevMaster() { if (!devMasterActive) { alert("🛡 DevMaster control is required to access this feature."); throw new Error("DevMaster protection blocked this action."); } }
-(function loadOverrideState() { const flag = localStorage.getItem("wizzOverride"); if (flag === "true") { overrideMode = true; } })();
-window.applyOverrideMode = applyOverrideMode; window.clearOverrideMode = clearOverrideMode; window.isOverrideModeActive = isOverrideModeActive; window.enforceDevMaster = enforceDevMaster;
